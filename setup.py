df9aea50f8126d980dafb55e6bf9208c7db42c62
Michael Denker <m.denker@fz-juelich.de>
2021-05-04 00:13:04 +0200

    Definitely, gnu++17, but new try.

--- setup.py
+++ setup.py
@@ -40,7 +40,8 @@ elif platform.system() == "Darwin":
             '-DMODULE_NAME=fim', '-DUSE_OPENMP', '-DWITH_SIG_TERM',
             '-Dfim_EXPORTS', '-O3', '-pedantic', '-Wextra',
             '-Weffc++', '-Wunused-result', '-Werror',
-            '-Xpreprocessor', '-fopenmp', '-std=c+1z'])
+            #'-Xpreprocessor',
+            '-fopenmp', '-std=gnu++17'])
 else:
     fim_module = Extension(
         name='elephant.spade_src.fim',
